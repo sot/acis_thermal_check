3d218edc39537c2f3a1a66ea26a1705a83243f5c
acisdude <acisdude@cfa.harvard.edu>
2016-06-23 17:10:49 -0400

    Add in the scripts

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 from setuptools import setup
+import glob
+
+scripts = glob.glob("scripts/*")
 
 setup(name='acis_thermal_check',
       packages=['acis_thermal_check'],
@@ -9,6 +12,7 @@ setup(name='acis_thermal_check',
       author_email='jzuhone@gmail.com',
       url='http://github.com/acisops/acis_thermal_check',
       download_url='https://github.com/acisops/acis_thermal_check/tarball/1.0.0',
+      scripts=scripts,
       classifiers=[
           'Intended Audience :: Science/Research',
           'Operating System :: OS Independent',
