7b3654b8420120c6e32c6b08ad3e27abc82fa3c2
acisdude <acisdude@cfa.harvard.edu>
2016-06-23 20:24:58 -0400

    Add in template and model spec files

--- setup.py
+++ setup.py
@@ -3,6 +3,8 @@ from setuptools import setup
 import glob
 
 scripts = glob.glob("scripts/*")
+templates = glob.glob("templates/*")
+model_spec = glob.glob("model_spec/*")
 
 setup(name='acis_thermal_check',
       packages=['acis_thermal_check'],
@@ -12,6 +14,8 @@ setup(name='acis_thermal_check',
       author_email='jzuhone@gmail.com',
       url='http://github.com/acisops/acis_thermal_check',
       download_url='https://github.com/acisops/acis_thermal_check/tarball/1.0.0',
+      data_files=[('templates', templates),
+                  ('model_spec', model_spec)],
       scripts=scripts,
       classifiers=[
           'Intended Audience :: Science/Research',
