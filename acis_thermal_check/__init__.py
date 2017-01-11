3ad6d97cace48a980e91c3242c27d557f7ea0db6
John ZuHone <jzuhone@gmail.com>
2017-01-11 10:33:48 -0500

    Remove thermal models from __init__

--- acis_thermal_check/__init__.py
+++ acis_thermal_check/__init__.py
@@ -5,11 +5,6 @@ from acis_thermal_check.main import \
 from acis_thermal_check.utils import \
     calc_off_nom_rolls, get_options
 
-from acis_thermal_check.dea_check import dea_check
-from acis_thermal_check.dpa_check import dpa_check
-from acis_thermal_check.psmc_check import psmc_check
-
-
 def test(*args, **kwargs):
     '''
     Run py.test unit tests.
