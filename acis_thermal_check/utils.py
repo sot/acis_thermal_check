3addaaf130e99fa6b108cd885345bc4a66e25974
John ZuHone <jzuhone@gmail.com>
2017-02-10 17:30:47 -0500

    Add comments to utilities

--- acis_thermal_check/utils.py
+++ acis_thermal_check/utils.py
@@ -9,6 +9,15 @@ from Ska.Matplotlib import cxctime2plotdate
 TASK_DATA = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 
 def calc_off_nom_rolls(states):
+    """
+    Calculate the off-nominal rolls from commanded states, which
+    include for a given state its start and stop times and the
+    attitude quaternion.
+
+    states : NumPy record array
+        The commanded states to compute the off-nominal rolls
+        from.
+    """
     off_nom_rolls = []
     for i, state in enumerate(states):
         att = [state[x] for x in ['q1', 'q2', 'q3', 'q4']]
@@ -17,9 +26,10 @@ def calc_off_nom_rolls(states):
     return np.array(off_nom_rolls)
 
 def globfile(pathglob):
-    """Return the one file name matching ``pathglob``.  Zero or multiple
-    matches raises an IOError exception."""
-
+    """
+    Return the one file name matching ``pathglob``. Zero or multiple
+    matches raises an IOError exception.
+    """
     files = glob.glob(pathglob)
     if len(files) == 0:
         raise IOError('No files matching %s' % pathglob)
@@ -29,9 +39,21 @@ def globfile(pathglob):
         return files[0]
 
 def config_logging(outdir, verbose, name):
-    """Set up file and console logger.
-    See http://docs.python.org/library/logging.html
-              #logging-to-multiple-destinations
+    """
+    Set up file and console logger.
+    See http://docs.python.org/library/logging.html#logging-to-multiple-destinations
+    Logs to the console and to run.dat.
+
+    Parameters
+    ----------
+    outdir : string
+        The location of the directory which the model outputs
+        are being written to.
+    verbose : integer
+        Indicate how verbose we want the logger to be.
+        (0=quiet, 1=normal, 2=debug)
+    name : string
+        The name of the ACIS component whose temperature is being modeled.
     """
     # Disable auto-configuration of root logger by adding a null handler.
     # This prevents other modules (e.g. Chandra.cmd_states) from generating
@@ -42,6 +64,7 @@ def config_logging(outdir, verbose, name):
     rootlogger = logging.getLogger()
     rootlogger.addHandler(NullHandler())
 
+    # Set numerical values for the different log levels
     loglevel = {0: logging.CRITICAL,
                 1: logging.INFO,
                 2: logging.DEBUG}.get(verbose, logging.INFO)
@@ -65,13 +88,50 @@ def plot_two(fig_id, x, y, x2, y2,
              color='blue', color2='magenta',
              ylim=None, ylim2=None,
              xlabel='', ylabel='', ylabel2='', title='',
-             figsize=(7, 3.5),
-             ):
-    """Plot two quantities with a date x-axis"""
+             figsize=(7, 3.5)):
+    """
+    Plot two quantities with a date x-axis, one on the left
+    y-axis and the other on the right y-axis.
+
+    Parameters
+    ----------
+    fig_id : integer
+        The ID for this particular figure.
+    x : NumPy array
+        Times in seconds since the beginning of the mission for
+        the left y-axis quantity.
+    y : NumPy array
+        Quantity to plot against the times on the left x-axis.
+    x2 : NumPy array
+        Times in seconds since the beginning of the mission for
+        the right y-axis quantity.
+    y2 : NumPy array
+        Quantity to plot against the times on the right y-axis.
+    linestyle : string, optional
+        The style of the line for the left y-axis.
+    linestyle2 : string, optional
+        The style of the line for the right y-axis.
+    color : string, optional
+        The color of the line for the left y-axis.
+    color2 : string, optional
+        The color of the line for the right y-axis.
+    xlabel : string, optional
+        The label of the x-axis.
+    ylabel : string, optional
+        The label for the left y-axis.
+    ylabel2 : string, optional
+        The label for the right y-axis.
+    title : string, optional
+        The title for the plot.
+    figsize : 2-tuple of floats
+        Size of plot in width and height in inches.
+    """
+    # Convert times to dates
     xt = cxctime2plotdate(x)
     fig = plt.figure(fig_id, figsize=figsize)
     fig.clf()
     ax = fig.add_subplot(1, 1, 1)
+    # Plot left y-axis
     ax.plot_date(xt, y, fmt='-', linestyle=linestyle, color=color)
     ax.set_xlim(min(xt), max(xt))
     if ylim:
@@ -81,8 +141,9 @@ def plot_two(fig_id, x, y, x2, y2,
     ax.set_title(title)
     ax.grid()
 
-    ax2 = ax.twinx()
+    # Plot right y-axis
 
+    ax2 = ax.twinx()
     xt2 = cxctime2plotdate(x2)
     ax2.plot_date(xt2, y2, fmt='-', linestyle=linestyle2, color=color2)
     ax2.set_xlim(min(xt), max(xt))
@@ -100,6 +161,26 @@ def plot_two(fig_id, x, y, x2, y2,
     return {'fig': fig, 'ax': ax, 'ax2': ax2}
 
 def get_options(msid, name, model_path, opts=None):
+    """
+    Construct the argument parser for command-line options. Sets up the
+    parser and defines default options. This function should be used by
+    the specific thermal model checking tools.
+
+    Parameters
+    ----------
+    msid : string
+        The MSID mnemomic for the temperature to be modeled.
+    name : string
+        The name of the ACIS component whose temperature is being modeled.
+    model_path : string
+        The default directory path where the model JSON files are located.
+        This is internal to the ``acis_thermal_check`` package.
+    opts: dictionary
+        A (key, value) dictionary of additional options for the parser. These
+        may be defined by the thermal model checking tool if necessary.
+    """
+    # At some point this should be changed to use
+    # argparse because optparse is deprecated
     from optparse import OptionParser
     parser = OptionParser()
     parser.set_defaults()
