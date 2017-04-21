c9a0313cd24d0ebf99562c2d728fb079ce475bfb
John ZuHone <jzuhone@gmail.com>
2017-04-21 18:18:57 -0400

    Use the actual beginning of the load from the backstop

--- acis_thermal_check/main.py
+++ acis_thermal_check/main.py
@@ -320,7 +320,7 @@ class ACISThermalCheck(object):
         plt.rc("ytick", labelsize=10)
         temps = {self.name: model.comp[self.msid].mvals}
         # make_prediction_plots runs the validation of the model against previous telemetry
-        plots = self.make_prediction_plots(opt.outdir, states, model.times, temps, tstart)
+        plots = self.make_prediction_plots(opt.outdir, states, model.times, temps, bs_cmds[0]['time'])
         # make_prediction_viols determines the violations and prints them out
         viols = self.make_prediction_viols(model.times, temps, tstart)
         # write_states writes the commanded states to states.dat
