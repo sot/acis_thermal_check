3c6bf5069cfc7643686ab181a727e878b5d91dce
John ZuHone <jzuhone@gmail.com>
2017-10-05 10:55:12 -0400

    Add HDF5StateBuilder to init

--- acis_thermal_check/__init__.py
+++ acis_thermal_check/__init__.py
@@ -4,7 +4,7 @@ from acis_thermal_check.main import \
     ACISThermalCheck
 from acis_thermal_check.commanded_states import \
     LegacyStateBuilder, ACISStateBuilder, \
-    state_builders
+    HDF5StateBuilder, state_builders
 from acis_thermal_check.utils import \
     calc_off_nom_rolls, get_options, \
     get_acis_limits
