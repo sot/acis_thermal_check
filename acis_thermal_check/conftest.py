a14782da369a37e830bc92a0552f04e1888d2db1
John ZuHone <jzuhone@gmail.com>
2018-09-10 15:17:36 -0400

    Add a fixture for the model specification file

--- acis_thermal_check/conftest.py
+++ acis_thermal_check/conftest.py
@@ -5,7 +5,14 @@ def pytest_addoption(parser):
                      help="Generate new answers, but don't test. "
                           "Argument is the directory to store the answers to. "
                           "Default: None, which performs the test.")
+    parser.addoption("--model_spec",
+                     help="The path to the model specification used by "
+                          "the model test run.")
 
 @pytest.fixture()
 def answer_store(request):
     return request.config.getoption('--answer_store')
+
+@pytest.fixture()
+def model_spec(request):
+    return request.config.getoption('--model_spec')
