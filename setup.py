5ce0b8cd2b0ee94a8b43f8461d78af108bac6a55
John ZuHone <jzuhone@gmail.com>
2020-02-10 13:21:55 -0500

    Remove unnecessary stuff in setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python
 from setuptools import setup
 import glob
-from acis_thermal_check import __version__
 
 templates = glob.glob("templates/*")
-
-url = 'https://github.com/acisops/acis_thermal_check/tarball/{}'.format(__version__)
 
 setup(name='acis_thermal_check',
       packages=["acis_thermal_check"],
@@ -18,10 +15,4 @@ setup(name='acis_thermal_check',
       download_url=url,
       data_files=[('templates', templates)],
       include_package_data=True,
-      classifiers=[
-          'Intended Audience :: Science/Research',
-          'Operating System :: OS Independent',
-          'Programming Language :: Python :: 2.7',
-          'Programming Language :: Python :: 3.5',
-      ],
       )
